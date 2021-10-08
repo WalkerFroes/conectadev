c826f1a6d212f48d46358c1af309d1d840b90af3
Fróes <walker_aragao@hotmail.com>
2021-10-08 19:23:46 -0300

    Inicio Header

--- src/pages/Home/components/Header.js
+++ src/pages/Home/components/Header.js
@@ -1,19 +1,43 @@
 import React from "react";
+import { Button } from '@material-ui/core'
+import { makeStyles } from '@material-ui/styles';
+import { AppBar } from "@material-ui/core";
+import { Toolbar } from "@material-ui/core";
+import { SvgIcon } from "@material-ui/core";
+
+const useStyles = makeStyles({
+    appBar: {
+        boxShadow: 'none',
+    },
+    img: {
+        maxHeight: 55,
+    }
+});
 
 function Header() {
+    const classes = useStyles();
+
     return (
-        <header className="header">
-        <div className="toolbar">
+        <AppBar position="fixed" color="inherit" className={classes.appBar}>
+        <Toolbar>
+            <img src="/images/logo.png" alt="logo" className={classes.img}/>
             <div>
+                <Button color='primary' variant='contained'>
+                    Novo Post
+                </Button>
+            </div>
+            {/* <div>
                 <a href='/'>Conecta Dev - Alteração</a>
             </div>
             <div>
-                <button>Novo Post</button>
+                <Button color='primary' variant='contained'>
+                    Novo Post
+                </Button>
                 <span>Img1</span>
                 <span>Img2</span>
-            </div>
-        </div>
-    </header>
+            </div> */}
+        </Toolbar>
+    </AppBar>
     )
 }
 
